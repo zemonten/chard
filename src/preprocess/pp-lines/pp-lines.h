7aff5a77dbf8414bfe049479d02b47443138d49f
zemonten <ratherzemonten@gmail.com>
2026-08-21 07:13:10 +0700

    unzipgit

---
+++ src/preprocess/pp-lines/pp-lines.h
@@ -0,0 +1,11 @@
+/* pp-lines.h -- auto-generated declarations for the 'preprocess_lines' module
+ * (1 function(s), defined in pp-lines.c). */
+#ifndef CHARD_MOD_PP_LINES_H
+#define CHARD_MOD_PP_LINES_H
+
+#include "../../chard_types.h"
+#include "../../chard_globals.h"
+
+void pp__push(const char *text, int orig_line_no);
+
+#endif /* CHARD_MOD_PP_LINES_H */
