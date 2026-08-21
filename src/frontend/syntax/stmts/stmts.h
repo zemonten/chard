1c1a46608c0ce8d7e452f96c8ba23c55926dbae8
zemonten <ratherzemonten@gmail.com>
2026-08-21 07:12:15 +0700

    unzipgit

---
+++ src/frontend/syntax/stmts/stmts.h
@@ -0,0 +1,11 @@
+/* stmts.h -- auto-generated declarations for the 'statements' module
+ * (1 function(s), defined in stmts.c). */
+#ifndef CHARD_MOD_STMTS_H
+#define CHARD_MOD_STMTS_H
+
+#include "../../../chard_types.h"
+#include "../../../chard_globals.h"
+
+int split__statements(const char *line, char ***out_chunks);
+
+#endif /* CHARD_MOD_STMTS_H */
