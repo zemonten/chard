e8a8e598713d57ac940907f7f7d331fd94589441
zemonten <ratherzemonten@gmail.com>
2026-08-21 07:13:19 +0700

    unzipgit

---
+++ src/core/decls/decls.h
@@ -0,0 +1,12 @@
+/* decls.h -- auto-generated declarations for the 'decls_table' module
+ * (2 function(s), defined in decls.c). */
+#ifndef CHARD_MOD_DECLS_H
+#define CHARD_MOD_DECLS_H
+
+#include "../../chard_types.h"
+#include "../../chard_globals.h"
+
+decl_t *find__decl(const char *name);
+func_sig_t *find_func_sig(const char *name);
+
+#endif /* CHARD_MOD_DECLS_H */
