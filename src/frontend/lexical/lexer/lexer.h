989ed39aa2767035e470a0e0dc7f135c3019bc79
zemonten <ratherzemonten@gmail.com>
2026-08-21 07:12:17 +0700

    unzipgit

---
+++ src/frontend/lexical/lexer/lexer.h
@@ -0,0 +1,11 @@
+/* lexer.h -- auto-generated declarations for the 'tokenizer' module
+ * (1 function(s), defined in lexer.c). */
+#ifndef CHARD_MOD_LEXER_H
+#define CHARD_MOD_LEXER_H
+
+#include "../../../chard_types.h"
+#include "../../../chard_globals.h"
+
+int tokenize(char *line, char *tokens[MAX_TOKENS]);
+
+#endif /* CHARD_MOD_LEXER_H */
